c86ed878ec140d239860b8a1dcda7bd31326d031
Ansh <kothariansh31@gmail.com>
2022-12-18 18:58:47 +0530

    Appplied Transition on splash screen and removed Title bar from splash

--- app/src/main/java/com/example/superquizzer/SplashActivity.kt
+++ app/src/main/java/com/example/superquizzer/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.example.superquizzer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        Handler((Looper.getMainLooper())).postDelayed({
+            val intent = Intent(this@SplashActivity,RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
