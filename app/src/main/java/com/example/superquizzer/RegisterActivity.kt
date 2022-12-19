885a8bb4e31d5e6419e887cd2edb672e03c7ffd0
Sumit Das <das.sumitsanjay03oo7@gmail.com>
2022-12-19 22:21:46 +0530

    added splashscreen, login transition and animation XML files

--- app/src/main/java/com/example/superquizzer/RegisterActivity.kt
+++ app/src/main/java/com/example/superquizzer/RegisterActivity.kt
@@ -1,11 +1,24 @@
 package com.example.superquizzer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var textlogin : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        supportActionBar?.hide()
+
+        textlogin = findViewById(R.id.login)
+
+        textlogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/superquizzer/SplashActivity.kt
+++ app/src/main/java/com/example/superquizzer/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +13,21 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         supportActionBar?.hide()
-        Handler((Looper.getMainLooper())).postDelayed({
-                 val intent = Intent(this@SplashActivity, RegisterActivity::class.java)
-            startActivity(intent)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val i = Intent(
+                this@SplashActivity,
+                RegisterActivity::class.java
+            )
+            startActivity(i)
             finish()
         }, 3000)
     }
+
 }
