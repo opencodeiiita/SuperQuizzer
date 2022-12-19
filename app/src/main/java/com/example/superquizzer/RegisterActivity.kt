07bc030f6ee7ab10dd2629a3ec589b1fea97870c
Sumit Das <das.sumitsanjay03oo7@gmail.com>
2022-12-19 22:42:01 +0530

    removed status bar

--- app/src/main/java/com/example/superquizzer/RegisterActivity.kt
+++ app/src/main/java/com/example/superquizzer/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.example.superquizzer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
@@ -13,6 +14,11 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         supportActionBar?.hide()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_register)
 
         textlogin = findViewById(R.id.login)
 
