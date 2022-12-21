363ecad86eea956ec896d9574939e064e083bcae
Sumit Das <das.sumitsanjay03oo7@gmail.com>
2022-12-21 21:25:14 +0530

    made changes

--- app/src/main/java/com/example/superquizzer/LoginActivity.kt
+++ app/src/main/java/com/example/superquizzer/LoginActivity.kt
@@ -1,13 +1,35 @@
 package com.example.superquizzer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
         supportActionBar?.hide()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_register)
+
+        var textSignup : TextView = findViewById(R.id.register_link)
+        var loginButton : Button = findViewById(R.id.login_button)
+
+        textSignup.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        loginButton.setOnClickListener {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
