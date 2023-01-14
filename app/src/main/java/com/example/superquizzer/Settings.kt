6cbb981b1dd93e45439753f9214a8c4561704fa5
SuhailAhmadKhan <suhailkhan568@gmail.com>
2023-01-14 16:59:13 +0530

    Changes

--- app/src/main/java/com/example/superquizzer/Settings.kt
+++ app/src/main/java/com/example/superquizzer/Settings.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
@@ -25,6 +26,7 @@ class Settings : AppCompatActivity() {
     @SuppressLint("UseSwitchCompatOrMaterialCode", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
             setTheme(R.style.darkTheme)
         } else {
